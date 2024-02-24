180cce8e366b07d9829fa4d8b1a98265589d6d56
aniiaham <aniya_axm@icloud.com>
2024-02-23 22:37:51 -0500

    first commit

---
+++ components/header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="py-5 bg-slate-300">
+      <div>
+        <Link href="/">
+          <h1 className="text-blue-700 font-bold text-5xl text-center">
+            FindRecipes
+          </h1>
+        </Link>
+      </div>
+    </div>
+  );
+}
