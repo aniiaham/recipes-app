310ec1e1d04c8638212820075f0f38df56d3004c
aniiaham <aniya_axm@icloud.com>
2024-02-28 12:03:37 -0500

    commit

--- app/types/[type]/[id]/page.tsx
+++ app/types/[type]/[id]/page.tsx
@@ -17,8 +17,8 @@ export default async function Page({ params }: { params: { id: string } }) {
     .map((ingKey) => details[ingKey])
     .filter(Boolean);
 
-  //   console.log(details);
-  console.log(ingredients);
+//   console.log(details);
+//   console.log(ingredients);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
       <div className="">
@@ -32,22 +32,48 @@ export default async function Page({ params }: { params: { id: string } }) {
         />
       </div>
       <div className="p-5">
-        <h1 className="text-2xl font-medium">
-          Recipe Name: <span>{details.strMeal}</span>
+        <h1 className="text-3xl font-bold font-mono text-blue-950">
+          {details.strMeal}
         </h1>
-        <div className="tags my-4 ">
-          <p className="text-2xl font-medium my-2">Ingredients:</p>
+        <div className="tags my-4">
+          <p className="text-2xl my-2  text-blue-950 font-mono font-semibold">
+            Ingredients
+          </p>
           {ingredients.map((ing, idx) => (
             <span
               key={idx}
-              className="bg-blue-500 text-white py-1 px-2 inline-block rounded mr-2 mb-2"
+              className="bg-blue-500 font-sans text-white text-base py-1 px-2 inline-block rounded mr-2 mb-2"
             >
               {ing}
             </span>
           ))}
         </div>
+        <div className="my-4">
+          <h3 className="text-2xl font-semibold my-2 font-mono text-blue-950">
+            Instructions
+          </h3>
+          <p className="font-sans text-base mr-2 mb-2">
+            {details.strInstructions}
+          </p>
+        </div>
+        {details.strYoutube && (
+          <div className="my-4">
+            <p className="text-2xl font-semibold my-2 font-mono text-blue-950">
+              Watch on YouTube
+            </p>
+            <a
+              href={details.strYoutube}
+              target="_blank"
+              className="font-sans text-base mr-2 mb-2"
+            >
+              How to make{" "}
+              <span className="underline text-blue-500">
+                {details.strYoutube}
+              </span>
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
-  return <div>Page</div>;
 }
